97c5cc491c708994f7ac6fda4e82b34661a2a174
AddyMills <74471839+AddyMills@users.noreply.github.com>
2026-01-22 10:20:49 -0600

    Refactor SongListManager

--- SongListManager/SongListManager.cs
+++ SongListManager/SongListManager.cs
@@ -1,14 +1,6 @@
-﻿using GH_Toolkit_Core.Methods;
-using GH_Toolkit_Core.QB;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows.Forms;
+using GH_Toolkit_Core.QB;
 using static GH_Toolkit_Core.Methods.CreateForGame;
 using static GH_Toolkit_Core.PAK.PAK;
 using static GH_Toolkit_Core.QB.QB;
@@ -17,31 +9,30 @@ using static GH_Toolkit_Core.QB.QBConstants;
 using static GH_Toolkit_Core.QB.QBStruct;
 using static GH_Toolkit_GUI.PreCompileChecks;
 using GH_Toolkit_Core.PAK;
-using static GH_Toolkit_Exceptions.Exceptions;
-using GH_Toolkit_Core.PS360;
 
 namespace GH_Toolkit_GUI
 {
     public partial class SongListManager : Form
     {
         private readonly string sghFileFilter = "SGH Files (*.sgh)|*.sgh|Zip Files (*.zip)|*.zip|All files (*.*)|*.*";
-        private Dictionary<string, QBStruct.QBStructData> MasterList = new Dictionary<string, QBStruct.QBStructData>();
-        private static UserPreferences Pref = UserPreferences.Default;
-        private string Game;
-        private string PakFile;
-        private Dictionary<string, PakEntry> QbPak;
-        private PakCompiler Compiler;
-        private PakEntry Songlist;
-        private Dictionary<string, QBItem> SongListEntries;
-        private QBArrayNode DlSongList;
-        private QBStructData DlSongListProps;
+        private readonly Dictionary<string, QBStructData> MasterList = new();
+        private static readonly UserPreferences Pref = UserPreferences.Default;
+        
+        private string Game = GAME_GH3;
+        private string PakFile = string.Empty;
+        private Dictionary<string, PakEntry> QbPak = new();
+        private PakCompiler Compiler = null!;
+        private PakEntry Songlist = null!;
+        private Dictionary<string, QBItem> SongListEntries = new();
+        private QBArrayNode DlSongList = null!;
+        private QBStructData DlSongListProps = null!;
         private PakEntry? DownloadQb;
         private Dictionary<string, QBItem>? DownloadQbEntries;
-        private QBStructData DownloadList;
-        private QBStructData Tier1;
-        private QBArrayNode SongArray;
-        private string SghPath = "";
-        private string SghFolder = "";
+        private QBStructData DownloadList = null!;
+        private QBStructData Tier1 = null!;
+        private QBArrayNode SongArray = null!;
+        private string SghPath = string.Empty;
+        private string SghFolder = string.Empty;
         private bool IsSwitching = false;
 
         public SongListManager()
@@ -58,6 +49,7 @@ namespace GH_Toolkit_GUI
                 songList.SetItemChecked(i, true);
             }
         }
+        
         private void SelectNone()
         {
             for (int i = 0; i < songList.Items.Count; i++)
@@ -68,82 +60,56 @@ namespace GH_Toolkit_GUI
 
         private void LoadSetlistHelper()
         {
-            if (gh3Radio.Checked)
-            {
-                Game = GAME_GH3;
-            }
-            else
-            {
-                Game = GAME_GHA;
-            }
+            Game = gh3Radio.Checked ? GAME_GH3 : GAME_GHA;
             LoadSetlist();
         }
 
-        private void loadSetlist_Click(object sender, EventArgs e)
-        {
-            LoadSetlistHelper();
-        }
+        private void loadSetlist_Click(object sender, EventArgs e) => LoadSetlistHelper();
 
-        private void loadSetlist2_Click(object sender, EventArgs e)
-        {
-            LoadSetlistHelper();
-        }
+        private void loadSetlist2_Click(object sender, EventArgs e) => LoadSetlistHelper();
 
         private void importSghFile_Click(object sender, EventArgs e)
         {
-            using (var dialog = new OpenFileDialog())
+            using var dialog = new OpenFileDialog
             {
-                dialog.Filter = sghFileFilter;
-                if (dialog.ShowDialog() == DialogResult.OK)
-                {
-                    SghPath = dialog.FileName;
-                }
-                else
-                {
-                    return;
-                }
-                LoadSGH();
+                Filter = sghFileFilter
+            };
+            
+            if (dialog.ShowDialog() != DialogResult.OK)
+            {
+                return;
             }
+            
+            SghPath = dialog.FileName;
+            LoadSGH();
         }
 
-        private void convertButton_Click(object sender, EventArgs e)
-        {
-            ConvertSongs();
-        }
+        private void convertButton_Click(object sender, EventArgs e) => ConvertSongs();
 
-        private void selectAllButton_Click(object sender, EventArgs e)
-        {
-            SelectAll();
-        }
+        private void selectAllButton_Click(object sender, EventArgs e) => SelectAll();
 
-        private void selectNoneButton_Click(object sender, EventArgs e)
-        {
-            SelectNone();
-        }
+        private void selectNoneButton_Click(object sender, EventArgs e) => SelectNone();
 
-        private void tabControl1_TabIndexChanged(object sender, EventArgs e)
-        {
-            ClearAll();
-        }
+        private void tabControl1_TabIndexChanged(object sender, EventArgs e) => ClearAll();
 
-        private void exportToSgh_Click(object sender, EventArgs e)
-        {
-            ExportSongs();
-        }
+        private void exportToSgh_Click(object sender, EventArgs e) => ExportSongs();
 
-        private void deleteSelected_Click(object sender, EventArgs e)
-        {
-            DeleteSongs();
-        }
+        private void deleteSelected_Click(object sender, EventArgs e) => DeleteSongs();
 
         private void restoreSetlistButton_Click(object sender, EventArgs e)
         {
-            var confirm = MessageBox.Show("Are you sure you want to restore the original DLC setlist? This will remove all custom songs from your setlist.", "Confirm Restore", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            var confirm = MessageBox.Show(
+                "Are you sure you want to restore the original DLC setlist? This will remove all custom songs from your setlist.",
+                "Confirm Restore",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Warning);
+            
             if (confirm == DialogResult.No)
             {
                 MessageBox.Show("Restore cancelled.", "Cancelled", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 return;
             }
+            
             Gh3PcCheck(Game, true);
             MessageBox.Show("Original BetterGH3 setlist is now restored.", "Setlist Restored", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
@@ -179,29 +145,30 @@ namespace GH_Toolkit_GUI
             SetAllOptions(false);
             ghaRadio.Enabled = false;
             tabControl1.SelectedIndex = 0;
-            if (consoleSelect.SelectedIndex == 0)
+            
+            switch (consoleSelect.SelectedIndex)
             {
-                SetAllOptions(true);
-                ghaRadio.Enabled = true;
-                gh3Radio.Checked = true;
+                case 0: // PC
+                    SetAllOptions(true);
+                    ghaRadio.Enabled = true;
+                    gh3Radio.Checked = true;
+                    break;
+                case 1: // Xbox 360
+                case 2: // PS3
+                    SetImportOptions(true);
+                    gh3Radio.Checked = true;
+                    break;
             }
-            else if (consoleSelect.SelectedIndex == 1)
-            {
-                SetImportOptions(true);
-                gh3Radio.Checked = true;
-            }
-            else if (consoleSelect.SelectedIndex == 2)
-            {
-                SetImportOptions(true);
-                gh3Radio.Checked = true;
-            }
+            
             IsSwitching = false;
         }
 
         private void gh3Radio_CheckedChanged(object sender, EventArgs e)
         {
             if (IsSwitching) return;
+            
             SetAllOptions(false);
+            
             if (ghaRadio.Checked)
             {
                 tabControl1.SelectedIndex = 2;
